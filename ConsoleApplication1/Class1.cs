29227f5b8ba8e2a7bba67ac6ddb4e83d22e7b7bd
Niko <Niko@NIKOPC>
2020-04-22 11:07:41 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Ship
+    {
+        public double price { get; set; }
+        public int age { get; set; }
+        public string port { get; set; }
+        public string name { get; set; }
+
+        public void GetShipInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Возраст: {age}| |Порт: {port}| |Цена: {price}|");
+        }
+    }
+
+    class Boat : Ship
+    {
+        public string type { get; set; }
+
+        public virtual void GetBoatInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Возраст: {age}| |Порт: {port}| |Цена: {price}| |Тип: {type}");
+        }
+        static void Sayhi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ship creiser = new Ship();
+            creiser.name = "Крейсер Аврора";
+            creiser.price = 750000000;
+            creiser.age = 120;
+            creiser.port = " Порт Ораниенбаума";
+
+            creiser.GetShipInfo();
+
+            Boat boat = new Boat();
+            boat.type = "Парусник";
+            boat.name = "Яхта Святого Вячеслава";
+            boat.port = "Берега амазонки";
+            boat.price = 100000;
+            boat.age = 1;
+
+
+            boat.GetBoatInfo();
+
+
+
+        }
+    }
+}
